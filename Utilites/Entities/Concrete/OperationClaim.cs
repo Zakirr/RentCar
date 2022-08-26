43fd2be0e02ff7b6de15acdd082b633dafc74382
Zakir Ferhad <zakirferhad@gmail.com>
2022-08-26 12:39:26 +0400

    app js

---
+++ Utilites/Entities/Concrete/OperationClaim.cs
@@ -0,0 +1,10 @@
+﻿using Melumatlar.Abstract;
+
+namespace CoreAndUtilites.Entities.Concrete
+{
+    public class OperationClaim:IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
